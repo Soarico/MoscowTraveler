03e79289d9e803e8c3a3771d1225afa631cfa4da
Maria Nepomyaschaya <marianepom555@gmail.com>
2025-03-21 22:13:01 +0300

    fixing bugs

--- src/main/kotlin/dto/SightInfo.kt
+++
@@ -1,9 +0,0 @@
-package demo.application.dto
-
-data class SightInfo (
-    val sightName : Int,
-    val photo : Photo,
-    val placeInRating : Int,
-    val averageRate : Double,
-    val description : String,
-)
